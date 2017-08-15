63ada591f66cac70baec2b090ba8ad2f79ff5436
Jorge Gallardo <jorge8486@gmail.com>
2017-08-14 21:51:46 -0400

    add note function reads in notes.json and prevents duplicate note titles from being created

--- app.js
+++ app.js
@@ -2,24 +2,28 @@ console.log("Starting Node Notes.")
 
 const fs = require('fs');
 const _ = require('lodash');
+const yargs = require('yargs');
 
 const notes = require('./notes.js');
 
-var command = process.argv[2];
+const argv = yargs.argv
+var command = argv._[0];
 console.log('Command:',command);
+console.log('Process:', process.argv);
+console.log('Yargs:', argv);
 
 switch(command) {
   case 'add':
-    console.log('Adding new note.');
+    notes.addNote(argv.title, argv.body);
     break;
   case 'list':
-    console.log('Listing all notes.');
+    notes.getAll();
     break;
   case 'read':
-    console.log('Reading note.');
+    notes.getNote(argv.title);
     break;
   case 'remove':
-    console.log('Removing note.');
+    notes.removeNote(argv.title);
     break;
   default:
     console.log('Unrecognized command.');
--- notes.js
+++ notes.js
@@ -1,6 +1,55 @@
 console.log("Starting notes.js");
+const fs = require('fs');
 
-module.exports.addNote = () => {
-  console.log('Add note.');
-  return 'New note';
+var addNote = (title, body) => {
+  //create an empty notes array that will store the array of note objects
+  var notes = [];
+  var note = {
+    title, body
+  };
+/*
+  The try statement lets you test a block of code for errors.
+  The catch statement lets you handle the error.
+  The throw statement lets you create custom errors.
+  The finally statement lets you execute code, after try and catch, regardless of the result.
+*/
+  try {
+    //try reading the data already in notes.json, parsing it as json (from a string) and storing it as notes
+    var notesString = fs.readFileSync('notes.json');
+    notes = JSON.parse(notesString);
+  //if an exception is thrown in the try block, state why.
+  } catch(e) {
+    console.log('The file notes.json was empty.');
+  }
+
+  //filter() method creates a new array with all elements that pass the test implemented by the provided function
+  var duplicateNotes = notes.filter((note) => note.title === title); //returns true if there is a duplicate note in the array
+//equivalent to notes.filter(function(note) { return note.title === title) };
+
+  //if length of duplicate notes is greater than 0 then we don't want to save it, because a note with that title already exists
+  if(duplicateNotes.length === 0) {
+    notes.push(note);
+    fs.writeFileSync('notes.json', JSON.stringify(notes));
+  } else {
+    console.log('A note with that title already exists.');
+  }
+};
+
+var getAll = () => {
+  console.log('Getting all notes.');
+};
+
+var getNote = (title) => {
+  console.log('Retrieving', title + '.')
+};
+
+var removeNote = (title) => {
+  console.log('Removing the', title, 'note.');
+};
+
+module.exports = {
+  addNote, // = addNote: addNote in ES6
+  getAll,
+  getNote,
+  removeNote
 };
